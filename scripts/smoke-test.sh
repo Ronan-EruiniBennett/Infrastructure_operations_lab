b7a122004db5b0d84c24e1dc71e2c7c0c976d609
Ronan-EruiniBennett <rheb99@icloud.com>
2026-06-03 15:12:15 +1000

    added clean up to smoke test

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -2,19 +2,48 @@
 
 echo "Starting Container..."
 
+sleep 0.5
+
 echo "Enter container name"
 read NAME
+
+sleep 0.5
 
 echo "Enter computer port"
 read PORT
 
+sleep 0.5
+
 echo "Container being created with name $NAME and on port $PORT"
 
+sleep 0.5
+
+echo "Container ID:"
 sudo docker run -d --name $NAME -p $PORT:5000 -t infra-lab
 
 if sudo docker ps --format '{{.Names}}' | grep -q "^${NAME}$"; then
 	echo "Container running"
+	echo "Would you like to test an endpoint? (y/n)"
+	read TEST
+	while [ "$TEST" == "y" ]; do
+		echo "Which API endpoint do you want to test? Include the / prefix (/,/health,/metrics,/version)"
+		read ENDPOINT
+		curl "localhost:${PORT}${ENDPOINT}"
+		sleep 0.5
+		echo "Would you like to test another endpoint? (y/n)"
+		read TEST
+	done
+	sleep 0.5
+	echo "Would you like to stop the container? (y/n)"
+	read STOP
+	if [ "$STOP" == "y" ]; then
+		sudo docker stop $NAME
+		sudo docker rm $NAME
+	else
+		echo "Container is still running, manual shutdown required"
+	fi
 else
 	echo "Container is not running"
 fi
- 
+
+
