3d5052e74608355ff31c017126ad3b00d25cc400
Ronan-EruiniBennett <rheb99@icloud.com>
2026-06-03 14:36:04 +1000

    fixed missing permission in smoke test

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Starting Container"
+echo "Starting Container..."
 
 echo "Enter container name"
 read NAME
@@ -8,9 +8,11 @@ read NAME
 echo "Enter computer port"
 read PORT
 
+echo "Container being created with name $NAME and on port $PORT"
+
 sudo docker run -d --name $NAME -p $PORT:5000 -t infra-lab
- 
-if docker ps --format '{{.Names}}' | grep -q "^${NAME}$"; then
+
+if sudo docker ps --format '{{.Names}}' | grep -q "^${NAME}$"; then
 	echo "Container running"
 else
 	echo "Container is not running"
